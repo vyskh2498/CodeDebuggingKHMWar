923ba54efe4e2e6e7bacc2a2f29e42c83ef314c2
vyskh2498 <35701490+vyskh2498@users.noreply.github.com>
2020-04-06 22:20:54 +0530

    Update R_Vysakh-Question2.py

--- R_Vysakh-Question2.py
+++ R_Vysakh-Question2.py
@@ -1,14 +1,14 @@
 for i in range(input()): 
-        word = raw_input()
-        word=word.upper() 
+        word = raw_input() #changed to raw_input
+        word=word.upper() #changed to word=word.upper()
         vowels = ['A','E','I','O','U']
-        count = 0 
+        count = 0 #initialised
         for j in range(1,len(word)): 
                 if word[j] in vowels: 
-                        if j == 0: 
-                                count=count+1
+                        if j == 0: #chnaged = to ==
+                                count=count+1 #changed from count++ to count=count+1
                 	elif word[j+1] in vowels: 
                                 break
                         else:
-				count=count+1
+				count=count+1 #changed from count++ to count=count+1
 		print (count)
