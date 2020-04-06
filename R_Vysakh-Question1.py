c63626ff0eb860a8ee1962d3a18d6c1cb0c63169
vyskh2498 <35701490+vyskh2498@users.noreply.github.com>
2020-04-06 22:15:45 +0530

    Add files via upload

---
+++ R_Vysakh-Question1.py
@@ -0,0 +1,22 @@
+#
+Input = [] 
+n = int(input("Enter number of elements : ")) 
+ans = 0
+for i in range(0, n): 
+    ele = int(input()) 
+    Input.append(ele)
+if(Input[0]<0):
+    prev =-1
+else:
+    prev=1
+for elem in Input: 
+	if elem ==0: 		
+		sign = -1
+	else: 
+		sign = elem / abs(elem) 
+
+	if sign == -1*prev: 
+		prev = sign 		
+		sign = sign + 1
+		
+print(sign)
