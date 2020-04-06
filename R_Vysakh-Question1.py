67bb3fe9ca78c0c3312639ef4525a27e3a6fe679
vyskh2498 <35701490+vyskh2498@users.noreply.github.com>
2020-04-06 22:17:30 +0530

    Update R_Vysakh-Question1.py

--- R_Vysakh-Question1.py
+++ R_Vysakh-Question1.py
@@ -1,7 +1,7 @@
 #
 Input = [] 
 n = int(input("Enter number of elements : ")) 
-ans = 0
+ans = 0 #initialised
 for i in range(0, n): 
     ele = int(input()) 
     Input.append(ele)
@@ -10,12 +10,12 @@ if(Input[0]<0):
 else:
     prev=1
 for elem in Input: 
-	if elem ==0: 		
+	if elem ==0: 	# changed = to ==	
 		sign = -1
 	else: 
 		sign = elem / abs(elem) 
 
-	if sign == -1*prev: 
+	if sign == -1*prev: #changed to -1*prev
 		prev = sign 		
 		sign = sign + 1
 		
